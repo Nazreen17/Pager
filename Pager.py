2b6c0783d00e25269ab169998d70ce9b3908fb72
Naznn <67914348+Nazreen17@users.noreply.github.com>
2022-09-25 16:54:22 +0530

    Add files via upload

--- Pager.py
+++ Pager.py
@@ -1,1 +1,19 @@
 import feedparser
+import notify2
+import os
+import time
+def parseFeed() :
+    f = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml")
+    ICON_PATH = os.getcwd() + "/icon.ico"
+    notify2.init('Pager')
+    for newsitem in f['items']:
+        n = notify2.Notification(newsitem['title'],newsitem['summary'],icon = ICON_PATH)
+        n.set_urgency(notify2.URGENCY_NORMAL)
+        n.show()
+        n.set_timeout(15000)
+        time.sleep(1200)
+        
+if __name__ == '__main__':
+    parseFeed()
+
+
